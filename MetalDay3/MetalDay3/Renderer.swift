358aa844835283a3357bc8ee4e4831fe8d1ce574
Miki Takahashi <math.micky.dtl@gmail.com>
2018-12-03 04:03:20 +0900

    license

---
+++ MetalDay3/MetalDay3/Renderer.swift
@@ -0,0 +1,9 @@
+//
+//  Renderer.swift
+//  MetalDay3
+//
+//  Created by MikiTakahashi on 2018/12/03.
+//  Copyright © 2018 MikiTakahashi. All rights reserved.
+//
+
+import Foundation
---
+++ MetalDay3/MetalDay3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalDay3
+//
+//  Created by MikiTakahashi on 2018/12/03.
+//  Copyright © 2018 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
