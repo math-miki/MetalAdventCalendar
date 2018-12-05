94f28326565e2dc80a4314618c65710145756e41
Miki Takahashi <math.micky.dtl@gmail.com>
2018-12-05 16:04:40 +0900

    initial commit

---
+++ MetalDay5/MetalDay5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalDay5
+//
+//  Created by MikiTakahashi on 2018/12/05.
+//  Copyright © 2018 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
